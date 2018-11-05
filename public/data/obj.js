9e50792c66786a4e2e43379f5e21addbcd55419e
pdhoward <patrick.howard@hotmail.com>
2018-11-05 16:07:06 -0500

    update

---
+++ public/data/obj.js
@@ -0,0 +1,30 @@
+
+
+exports.obj = {
+  MessageSid: uuidv1(),
+  SmsSid:uuidv1(),
+  AccountSid: uuidv1(),
+  MessagingServiceSid: uuidv1(),
+  From: user,
+  To: platform,
+  Body: "",
+  NumMedia: "",
+  NumSegments: "",
+  MediaContentType: " ",
+  MediaUrl: " ",
+  FromCity:"Charlotte",
+  FromState: "NC",
+  FromZip: "28222",
+  FromCounty: "USA",
+  SmsStatus: "",
+  ToCity: "Charlotte",
+  ToState: "NC",
+  ToZip: "28222",
+  ToCountry: "USA",
+  AddOns: " ",
+  ApiVersion: "v1",
+  PostDate: Date.now(),
+  ChaoticSid: uuidv1(),
+  ChaoticSource: "web",
+  Token: undefined
+}
